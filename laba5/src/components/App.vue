ce7fedb10e71504ceac258d5f0cf58eaf474af2e
Slizis <79917720+Slizis@users.noreply.github.com>
2021-06-16 14:14:45 +0500

    Add files via upload

---
+++ laba5/src/components/App.vue
@@ -0,0 +1,119 @@
+<template actionBarHidden="true">
+  <Page actionBarHidden="true" class="app">
+    <StackLayout>
+      <TextField class="input" v-model="newText" hint=""  @returnPress= "newTask()"/>
+      <ScrollView orientation="horizontal">
+        <ListView  class="task" for="task in tasks">
+          <v-template>
+            <GridLayout columns="220%, 70, 70">
+            <label  class="task-text done" v-if="task.done" textWrap="true" col="0">{{task.title}}</label>
+            <label  class="task-text" v-else  @tap="edit(task.id, task.title)" textWrap="true" col="0">{{task.title}}</label>
+            <Button  class="btn check-btn" text="V" @tap="taskDone(task.id)" col="1"/>
+            <Button  class="btn remove-btn" text="X" @tap="remove(task.id)" col="2"/> 
+            </GridLayout>
+          </v-template>
+        </ListView>
+      </ScrollView>
+    </StackLayout>
+  </Page>
+</template>
+
+<script>
+import * as ApplicationSettings from "application-settings";
+export default {
+  data () {
+    return {
+      newText: '',
+      tasks: []
+    }
+  },
+  mounted(){
+    if(ApplicationSettings.getString('tasks')){
+      this.tasks=Object.values(JSON.parse(ApplicationSettings.getString('tasks')));
+    }
+  },
+  methods: {
+    newTask () {
+      if(this.newText != ''){
+        this.tasks.push({
+          id: Math.random(),
+          title: this.newText,
+          done: false
+        });
+        this.newText = '';
+      }
+      this.save();
+    },
+    taskDone (id) {
+      this.tasks = this.tasks.map(task => {
+        if (task.id == id) task.done = !task.done;
+        return task;
+      })
+      this.save();
+    },
+    remove (id) {
+      this.tasks = this.tasks.filter(task => task.id !== id);
+      this.save();
+    },
+    save(){
+      let toSave = Object.assign({}, this.tasks);
+      ApplicationSettings.setString('tasks', JSON.stringify(toSave));
+    },
+    
+    edit(id, old_text) {
+      prompt({
+        title: "Изменение задачи",
+        message: "Новая задача:",
+        okButtonText: "OK",
+        cancelButtonText: "Отмена",
+        defaultText: old_text,
+      })
+      .then(result => {
+         this.tasks.forEach(task => {
+          if (task.id == id && result.text != ''){
+            task.title = result.text;
+            this.save();
+          }    
+         });
+      })
+    }
+  }
+}
+</script>
+
+<style>
+.app{
+    background-color: #fff;
+}
+.task-text{
+  margin: 30px 20px;
+  background-color: #936cd6;
+  border-radius: 10%;
+  color: #0f0c0c;
+  text-align: center;
+}
+.done {
+  text-decoration: line-through;
+  background-color: #7d940d;
+}
+.btn{
+  background-color: #936cd6;
+  border-radius: 10%;
+  color: #ffffff;
+  margin: 30px 10px;
+}
+.btn:active {
+  background-color: #696969;
+}
+.input{
+  background-color: #936cd6;
+  border-radius: 10%;
+  color: #0f0c0c;
+  margin: 50px 30px;
+  text-align: center;
+}
+.input:focus{
+    background-color: #936cd6;
+}
+</style>
+
